ac8e93b3f2f37f69ea4fb2b0fc34505849ca1c3e
owaisanwar <notowais09@gmail.com>
2022-06-16 15:26:02 +0500

    problem 3 solved

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -3,6 +3,7 @@ Write a recursive function `iceCreamShop(flavors, favorite)` that takes in an
 array of ice cream flavors available at the ice cream shop, as well as the
 user's favorite ice cream flavor. Recursively find out whether or not the shop
 offers their favorite flavor.
+// PseudoCode practise by me:
 
 Examples:
 iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
@@ -11,9 +12,24 @@ iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea
 iceCreamShop(['moose tracks'], 'moose tracks'); // true
 iceCreamShop([], 'honey lavender'); // false
 ***********************************************************************/
+['vanilla' , 'xyz'];
 
 
 // your code here
+let iceCreamShop = function (flavors, favorite) {
+  console.log(flavors);
+  if (flavors.length === 0) {
+    return false;
+  }
+  if(flavors[0] === favorite) {
+    return true;
+  }
+
+  return flavors[0] === favorite ||  iceCreamShop(flavors.slice(1), favorite)
+
+}
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
+iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
