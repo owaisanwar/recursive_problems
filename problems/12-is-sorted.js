a21e01e44dc5e29e0c0e883549d83e86e2aaab7c
owaisanwar <notowais09@gmail.com>
2022-06-17 07:44:15 +0500

    solve problem 12 array isSorted

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -9,8 +9,22 @@ isSorted([1, 2, 4, 3, 5]); // false
 isSorted([2, 4, 6, 7, 8]); // true
 isSorted([5, 4, 3, 2, 1]); // false
 ***********************************************************************/
-
+1 === 2 - 1
 // your code here
+let isSorted = (arr) => {
+    if (arr.length === 1) {
+      return true;
+    }
+    if(arr.length > 1) {
+      return arr[0] < arr[1] && isSorted(arr.slice(1));
+    }
+
+}
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 2])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
